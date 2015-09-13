e6763827e5f02e400254d7a03940ad034429f565
Cameron Davidson-Pilon <cam.davidson.pilon@gmail.com>
2015-09-13 11:29:28 -0400

    adding file name and repo to SO github question scraper, so I can dedup later

--- scrapers/stackoverflow_questions_on_github_scraper.py
+++ scrapers/stackoverflow_questions_on_github_scraper.py
@@ -5,6 +5,7 @@ from requests import get
 from bs4 import BeautifulSoup
 import time
 import os
+import csv
 
 """
 Example search: https://github.com/search?l=python&o=desc&q=+%22stackoverflow.com%2Fquestions%22+language%3APython&ref=searchresults&s=indexed&type=Code&utf8=%E2%9C%93
@@ -20,11 +21,16 @@ def yield_code_from_page(page):
     github_raw_content_url = 'https://raw.githubusercontent.com'
     html = get(search_results_page_url(page)).text
     soup = BeautifulSoup(html)
-    results = soup.find_all(title=re.compile('\.py$'))
+    results = soup.find_all('p', class_='title')
 
-    for a_tag in results:
-        raw_href = a_tag.attrs['href'].replace('blob/', '')
-        yield get(github_raw_content_url + raw_href).text
+    for result in results:
+        a_tags = result.find_all('a')
+        repo = a_tags[0].text
+        filename = a_tags[1].text
+        if not filename.endswith('.py'):
+            continue
+        raw_href = a_tags[1].attrs['href'].replace('blob/', '')
+        yield repo, filename, get(github_raw_content_url + raw_href).text
 
 def find_stackoverflow_questions_in_file(code):
     so_regex = re.compile('stackoverflow.com/questions/[\d]+')
@@ -36,11 +42,13 @@ def find_stackoverflow_questions_in_file(code):
 def run(max_page=99, save_location='/data/github_stackover_flow_questions.csv'):
     save_location = os.path.dirname(os.path.realpath(__file__)) + save_location
     with open(save_location, 'a') as open_file:
+        writer = csv.writer(open_file)
+
         for i in range(1, max_page + 1):
             time.sleep(5)
-            for code in yield_code_from_page(i):
+            for repo, filename, code in yield_code_from_page(i):
                 for link in find_stackoverflow_questions_in_file(code):
-                    open_file.write(link + "\n")
+                    writer.writerow([repo, filename, link])
             print "Completed page %d" % i
 
     print "Saved to %s" % save_location
