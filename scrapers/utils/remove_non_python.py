24c1ec3fa4f35a3a4a477fc716c53e5da4e66e77
Cameron Davidson-Pilon <cam.davidson.pilon@gmail.com>
2015-09-20 21:40:52 -0400

    remove nonpython from pypi folders. Also, scheduale this in cron

--- scrapers/utils/remove_non_python.py
+++ scrapers/utils/remove_non_python.py
@@ -1,13 +1,18 @@
 # run a script that removes all non-python files, recursively. 
 import os
-target_folder = './scrapers/data/raw_repos/'
 
+
+PATH = os.path.dirname(os.path.realpath(__file__))
+
+target_folders = [PATH + '/../data/raw_repos/', PATH + '/../data/pypi/unpacked']
 
 total = 0
-for root, dirs, files in os.walk(target_folder):
-    for file in files:
-        if not file.endswith('.py'):
-            total += 1
-            os.remove(os.path.join(root, file))
+for folder in target_folders:
+    print folder
+    for root, dirs, files in os.walk(folder):
+        for file in files:
+            if not file.endswith('.py'):
+                total += 1
+                os.remove(os.path.join(root, file))
 
 print "Removed %d non-python files" % total
